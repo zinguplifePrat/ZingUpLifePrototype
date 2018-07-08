ece62df3cb52d158fd9f1521cf3c004f97fa84d6
shakti <svpsctg11@yahoo.com>
2018-07-08 14:40:51 +0530

    adds simple dynamic property to swiper

--- src/Wellness_Screens/pages/intro/index.js
+++ src/Wellness_Screens/pages/intro/index.js
@@ -14,18 +14,30 @@ import Page_FOUR from './slide_four';
 import Page_FIVE from './slide_five';
 import Page_SIX from './slide_six';
 export default class Index extends Component {
+  checkEnd(index){
+    if(index==4){
+      this.props.navigation.navigate("DrawerOpen");
+    }
+  }
   render(){
+    const classState =this;
     return (
-      <Swiper style={styles.wrapper} showsButtons={false}>
+      <Swiper style={styles.wrapper} showsButtons={false} loop={false} autoplay={true} autoplayTimeout={2} 
+      onIndexChanged={(index)=>{
+        if(index==4){
+          setTimeout(function(){
+            classState.props.navigation.navigate("DrawerOpen");
+          },2000);
+          
+        }
+      }}>
        <Page_ONE />
        <Page_TWO />
        <Page_THREE />
        <Page_FOUR />
        <Page_FIVE />
-       <Page_SIX />
+       {/* <Page_SIX /> */}
       </Swiper>
     );
   }
 }
-
-AppRegistry.registerComponent('myproject', () => Swiper);
